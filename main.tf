e639561088432e895121735e5d6edb0557508362
Abhishek <abhinit06@gmail.com>
2023-01-10 09:26:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,31 @@
 terraform {
+    # Here we configure the providers we need to run our configuration
   required_providers {
     google = {
       source = "hashicorp/google"
       version = "3.51.0"
     }
   }
-}
 
 # With this backend configuration we are telling Terraform that the
 # created state should be saved in some Google Cloud Bucket with some prefix
-backend "gcs" {
-  ## INSERT YOUR BUCKET HERE!!
-  bucket = "tf-state1212"
-  prefix = "terraform/state"
-  credentials = "terraform-sa.json"
-  }
+ backend "gcs" {
+   ## INSERT YOUR BUCKET HERE!!
+   bucket = "tf-state1212"
+   prefix = "terraform/state"
+   credentials = "terraform-sa.json"
+ }
 }
 
+
+# We define the "google" provider with the project and the general region + zone
 provider "google" {
   project = "gitops-trial"
   region  = "us-central1"
   zone    = "us-central1-a"
 }
 
+#Here we define a very small compute instance
 resource "google_compute_instance" "default" {
   name = "terraform-test-instance"
   machine_type = "f1-micro"
